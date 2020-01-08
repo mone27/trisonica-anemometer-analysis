a07ac0e555efb8b07f96c483084efbbc3c41c66a
Simone Massaro (mone27) <massaro.simone.it@gmail.com>
2020-01-08 15:19:30 +0100

    added preprocessing to invert u axes of TRS1 before EddyPro. Updated accordingly EP project files and generate cached data

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,8 +2,11 @@
 
 from pathlib import Path
 import numpy as np
+from multiprocessing import Pool
+import logging as log
+log.basicConfig(level=log.INFO)
 name_re = r"*_TRS_M00162_com3.raw"
-in_dir = Path("/run/media/simone/Simone DATI/TRISONICA_DATA/FIELD_DATA/agosto_settembra_all")
+in_dir = Path("/run/media/simone/Simone DATI/TRISONICA_DATA/FIELD_DATA/aug_sept_all")
 out_dir = Path("/run/media/simone/Simone DATI/TRISONICA_DATA/Processed/TRS1_all_data_fixed_axes")
 
 def process_trs1(data):
@@ -23,15 +26,19 @@ def process_trs1(data):
     return data
 
 def extract(f):
-    data = np.genfromtxt(f, usecols=(5, 7, 9, 11))
+    # log.info(f"opening {f}")
+    data = np.genfromtxt(f, usecols=(5, 7, 9, 11), invalid_raise=False)
     out_name = out_dir / f"{f.name[:-4]}_fixed_axes.csv"
     data = process_trs1(data)
     np.savetxt(out_name, data, header="u, v, w, t", delimiter=',', fmt='%2.2f')
+    log.info(f"saved file {out_name}")
 
 def main():
-    # possible optmization can use multithreads
-    for f in in_dir.glob(name_re):
-        extract(f)
+    if not out_dir.is_dir(): out_dir.mkdir(parents=True, exist_ok=True)
+    with Pool() as p:
+        p.map(extract, in_dir.glob(name_re))
+    # for f in in_dir.glob(name_re):
+    #     extract(f)
 
 
 if __name__ == '__main__':
